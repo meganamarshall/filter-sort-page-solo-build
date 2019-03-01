9cf22cd095a7249b2b49f36e6ddccf94a246ec7f
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-01 15:59:41 -0800

    passing initial filtering test

--- test/filter-contacts.test.js
+++ test/filter-contacts.test.js
@@ -2,6 +2,12 @@ const test = QUnit.test;
 
 QUnit.module('filtering logic');
 
+function filterContacts(contacts, filter) {
+    return contacts.filter(contact => {
+        const nameMatches = contact.name === filter.name;
+        return nameMatches;
+    });
+}
 test('filter by last name', assert => {
     //arrange
     const contacts = [
