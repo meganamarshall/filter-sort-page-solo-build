5033bfcdfd432a5cf6ccec08fb15578cc5de0158
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-01 16:50:38 -0800

    wrote/passing test for sort by id number

--- test/sort-contacts.test.js
+++ test/sort-contacts.test.js
@@ -19,6 +19,18 @@ function sortByName(contacts) {
         return -1;
     });
 }
+
+function sortByNumber(contacts) {
+    return contacts.sort((a, b) => {
+        if(a.id === b.id) {
+            return 0;
+        }
+        if(a.id > b.id) {
+            return 1;
+        }
+        return -1;
+    });
+}
 test('sort by first name', assert => {
     //arrange
     const expected = [
@@ -28,6 +40,19 @@ test('sort by first name', assert => {
     ];
     //act
     const sorted = sortByName(contacts);
+    //assert
+    assert.deepEqual(sorted, expected);
+});
+
+test('sort by id number', assert => {
+    //arrange
+    const expected = [
+        { first_name: 'megan', last_name: 'marshall', id: '1' },
+        { first_name: 'erin', last_name: 'marshall', id: '3' },
+        { first_name: 'patrick', last_name: 'posner', id: '4' }
+    ];
+    //act
+    const sorted = sortByNumber(contacts);
     //assert
     assert.deepEqual(sorted, expected);
 });
