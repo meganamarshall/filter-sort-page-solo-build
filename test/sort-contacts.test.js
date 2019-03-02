579c5b3602533ec6f9ca63930b76b439251bb524
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-01 16:54:17 -0800

    combined sorting into one function

--- test/sort-contacts.test.js
+++ test/sort-contacts.test.js
@@ -8,51 +8,44 @@ const contacts = [
     { first_name: 'erin', last_name: 'marshall', id: '3' }
 ];
 
-function sortByName(contacts) {
+function sortContacts(contacts, sortChoice) {
+    const property = sortChoice.property;
     return contacts.sort((a, b) => {
-        if(a.first_name === b.first_name) {
+        if(a[property] === b[property]) {
             return 0;
         }
-        if(a.first_name > b.first_name) {
+        if(a[property] > b[property]) {
             return 1;
         }
         return -1;
     });
 }
 
-function sortByNumber(contacts) {
-    return contacts.sort((a, b) => {
-        if(a.id === b.id) {
-            return 0;
-        }
-        if(a.id > b.id) {
-            return 1;
-        }
-        return -1;
-    });
-}
+
 test('sort by first name', assert => {
     //arrange
+    const sortChoice = { property: 'first_name' };
     const expected = [
         { first_name: 'erin', last_name: 'marshall', id: '3' },
         { first_name: 'megan', last_name: 'marshall', id: '1' },
         { first_name: 'patrick', last_name: 'posner', id: '4' }
     ];
     //act
-    const sorted = sortByName(contacts);
+    const sorted = sortContacts(contacts, sortChoice);
     //assert
     assert.deepEqual(sorted, expected);
 });
 
 test('sort by id number', assert => {
     //arrange
+    const sortChoice = { property: 'id' };
     const expected = [
         { first_name: 'megan', last_name: 'marshall', id: '1' },
         { first_name: 'erin', last_name: 'marshall', id: '3' },
         { first_name: 'patrick', last_name: 'posner', id: '4' }
     ];
     //act
-    const sorted = sortByNumber(contacts);
+    const sorted = sortContacts(contacts, sortChoice);
     //assert
     assert.deepEqual(sorted, expected);
 });
